476975ade1a6c07bee734af90cc031d969cfb65f
Or Novogroder <or.novogroder@coralogix.com>
2023-09-19 09:25:47 +0300

    patch - refactor alert controller and adding tests

--- controllers/alphacontrollers/alert_controller_test.go
+++ controllers/alphacontrollers/alert_controller_test.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"testing"
 
+	utils "github.com/coralogix/coralogix-operator/apis"
 	coralogixv1alpha1 "github.com/coralogix/coralogix-operator/apis/coralogix/v1alpha1"
 	alerts "github.com/coralogix/coralogix-operator/controllers/clientset/grpc/alerts/v2"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/types/known/wrapperspb"
+	"k8s.io/utils/pointer"
 )
 
 func TestFlattenAlerts(t *testing.T) {
 	alert := &alerts.Alert{
-		Id:          wrapperspb.String("id"),
-		Name:        wrapperspb.String("name"),
-		Description: wrapperspb.String("description"),
-		IsActive:    wrapperspb.Bool(true),
-		Severity:    alerts.AlertSeverity_ALERT_SEVERITY_CRITICAL,
+		UniqueIdentifier: wrapperspb.String("id"),
+		Name:             wrapperspb.String("name"),
+		Description:      wrapperspb.String("description"),
+		IsActive:         wrapperspb.Bool(true),
+		Severity:         alerts.AlertSeverity_ALERT_SEVERITY_CRITICAL,
+		MetaLabels:       []*alerts.MetaLabel{{Key: wrapperspb.String("key"), Value: wrapperspb.String("value")}},
 		Condition: &alerts.AlertCondition{
 			Condition: &alerts.AlertCondition_MoreThanUsual{
 				MoreThanUsual: &alerts.MoreThanUsualCondition{
@@ -33,6 +36,9 @@ func TestFlattenAlerts(t *testing.T) {
 				},
 			},
 		},
+		Filters: &alerts.AlertFilters{
+			FilterType: alerts.AlertFilters_FILTER_TYPE_METRIC,
+		},
 	}
 
 	spec := coralogixv1alpha1.AlertSpec{
@@ -43,16 +49,31 @@ func TestFlattenAlerts(t *testing.T) {
 	status, err := flattenAlert(context.Background(), alert, spec)
 	assert.NoError(t, err)
 
-	id := "id"
-	subgroupId := "subgroup_id"
+	minNonNullValuesPercentage := 10
 	expected := &coralogixv1alpha1.AlertStatus{
-		ID:          &id,
+		ID:          pointer.String("id"),
 		Name:        "name",
 		Description: "description",
 		Active:      true,
-		Severity:    "critical",
-		Condition:   "more_than_usual",
+		Severity:    "Critical",
+		Labels:      map[string]string{"key": "value"},
+		AlertType: coralogixv1alpha1.AlertType{
+			Metric: &coralogixv1alpha1.Metric{
+				Promql: &coralogixv1alpha1.Promql{
+					SearchQuery: "http_requests_total{status!~\"4..\"}",
+					Conditions: coralogixv1alpha1.PromqlConditions{
+						AlertWhen:                   "MoreThanUsual",
+						Threshold:                   utils.FloatToQuantity(3.0),
+						TimeWindow:                  coralogixv1alpha1.MetricTimeWindow("TwelveHours"),
+						MinNonNullValuesPercentage:  &minNonNullValuesPercentage,
+						ReplaceMissingValueWithZero: false,
+					},
+				},
+			},
+		},
+		NotificationGroups: []coralogixv1alpha1.NotificationGroup{},
+		PayloadFilters:     []string{},
 	}
 
-	assert.Equal(t, expected, status)
+	assert.EqualValues(t, expected, status)
 }
