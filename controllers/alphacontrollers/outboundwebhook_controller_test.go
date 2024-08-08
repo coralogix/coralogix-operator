c05446530131d60af093b2bc4be92a622673651c
Or Novogroder <108669655+OrNovo@users.noreply.github.com>
2024-08-08 14:43:46 +0300

    refactor webhooks controller (#130)
    * refactor webhooks controller

--- controllers/alphacontrollers/outboundwebhook_controller_test.go
+++ controllers/alphacontrollers/outboundwebhook_controller_test.go
@@ -175,22 +175,6 @@ func TestOutboundWebhookUpdate(t *testing.T) {
 						},
 					},
 				}, nil)
-				params.outboundWebhooksClient.EXPECT().GetOutboundWebhook(params.ctx, gomock.Any()).Return(&ow.GetOutgoingWebhookResponse{
-					Webhook: &ow.OutgoingWebhook{
-						Id:   wrapperspb.String("id"),
-						Name: wrapperspb.String("name"),
-						Type: ow.WebhookType_GENERIC,
-						Url:  wrapperspb.String("url"),
-						Config: &ow.OutgoingWebhook_GenericWebhook{
-							GenericWebhook: &ow.GenericWebhookConfig{
-								Uuid:    wrapperspb.String("uuid"),
-								Method:  ow.GenericWebhookConfig_GET,
-								Headers: map[string]string{"key": "value"},
-								Payload: wrapperspb.String("payload"),
-							},
-						},
-					},
-				}, nil)
 				params.outboundWebhooksClient.EXPECT().UpdateOutboundWebhook(params.ctx, gomock.Any()).Return(&ow.UpdateOutgoingWebhookResponse{}, nil)
 				params.outboundWebhooksClient.EXPECT().GetOutboundWebhook(params.ctx, gomock.Any()).Return(&ow.GetOutgoingWebhookResponse{
 					Webhook: &ow.OutgoingWebhook{
@@ -313,6 +297,106 @@ func TestOutboundWebhookUpdate(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 			}
+		})
+	}
+}
+
+func TestOutboundWebhookDeletion(t *testing.T) {
+	tests := []struct {
+		name            string
+		params          func(params PrepareOutboundWebhooksParams)
+		outboundWebhook coralogixv1alpha1.OutboundWebhook
+		shouldFail      bool
+	}{
+		{
+			name:       "outbound-webhook deletion success",
+			shouldFail: false,
+			params: func(params PrepareOutboundWebhooksParams) {
+				params.outboundWebhooksClient.EXPECT().CreateOutboundWebhook(params.ctx, gomock.Any()).Return(&ow.CreateOutgoingWebhookResponse{Id: wrapperspb.String("id")}, nil)
+				params.outboundWebhooksClient.EXPECT().GetOutboundWebhook(params.ctx, gomock.Any()).Return(&ow.GetOutgoingWebhookResponse{
+					Webhook: &ow.OutgoingWebhook{
+						Id:   wrapperspb.String("id"),
+						Name: wrapperspb.String("name"),
+						Type: ow.WebhookType_GENERIC,
+						Url:  wrapperspb.String("url"),
+						Config: &ow.OutgoingWebhook_GenericWebhook{
+							GenericWebhook: &ow.GenericWebhookConfig{
+								Uuid:    wrapperspb.String("uuid"),
+								Method:  ow.GenericWebhookConfig_GET,
+								Headers: map[string]string{"key": "value"},
+								Payload: wrapperspb.String("payload"),
+							},
+						},
+					},
+				}, nil)
+				params.outboundWebhooksClient.EXPECT().DeleteOutboundWebhook(params.ctx, gomock.Any()).Return(&ow.DeleteOutgoingWebhookResponse{}, nil)
+			},
+			outboundWebhook: coralogixv1alpha1.OutboundWebhook{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "outbound-webhook-deletion-success",
+					Namespace: "default",
+				},
+				Spec: coralogixv1alpha1.OutboundWebhookSpec{
+					Name: "name",
+					OutboundWebhookType: coralogixv1alpha1.OutboundWebhookType{
+						GenericWebhook: &coralogixv1alpha1.GenericWebhook{
+							Url:     "url",
+							Method:  "Get",
+							Headers: map[string]string{"key": "value"},
+							Payload: pointer.String("payload"),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			outboundWebhooksClient := mock_clientset.NewMockOutboundWebhooksClientInterface(controller)
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			if tt.params != nil {
+				tt.params(PrepareOutboundWebhooksParams{
+					ctx:                    ctx,
+					outboundWebhooksClient: outboundWebhooksClient,
+				})
+			}
+
+			reconciler, watcher := setupOutboundWebhooksReconciler(t, ctx, outboundWebhooksClient)
+
+			err := reconciler.Client.Create(ctx, &tt.outboundWebhook)
+
+			assert.NoError(t, err)
+
+			<-watcher.ResultChan()
+
+			_, err = reconciler.Reconcile(ctx, ctrl.Request{
+				NamespacedName: types.NamespacedName{
+					Namespace: tt.outboundWebhook.Namespace,
+					Name:      tt.outboundWebhook.Name,
+				},
+			})
+
+			err = reconciler.Client.Delete(ctx, &tt.outboundWebhook)
+
+			assert.NoError(t, err)
+
+			<-watcher.ResultChan()
+
+			_, err = reconciler.Reconcile(ctx, ctrl.Request{
+				NamespacedName: types.NamespacedName{
+					Namespace: tt.outboundWebhook.Namespace,
+					Name:      tt.outboundWebhook.Name,
+				},
+			})
+
+			assert.NoError(t, err)
 		})
 	}
 }
