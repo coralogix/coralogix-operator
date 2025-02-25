daa6658a21c38ba74b0e9e1963d1a3fe8ead3c75
Or Novogroder <or.novogroder@coralogix.com>
2025-02-25 12:05:38 +0200

    fixes

--- api/coralogix/v1alpha1/scope_types.go
+++ api/coralogix/v1alpha1/scope_types.go
@@ -92,7 +92,7 @@ func (s *ScopeSpec) ExtractScopeFilters() ([]*cxsdk.ScopeFilter, error) {
 // ScopeStatus defines the observed state of Scope.
 type ScopeStatus struct {
 	// +optional
-	ID *string `json:"id"`
+	ID *string `json:"id,omitempty"`
 	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
