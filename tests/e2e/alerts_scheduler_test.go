43c43ff7696cbfce43c52b9f69348909d8c78593
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-04-04 12:16:53 +0200

    fix: rebase

--- tests/e2e/alerts_scheduler_test.go
+++ tests/e2e/alerts_scheduler_test.go
@@ -145,21 +145,13 @@ func getSampleAlertScheduler(name, namespace string) *coralogixv1alpha1.AlertSch
 			Name:        name,
 			Description: "This is a sample alert scheduler",
 			Enabled:     true,
-<<<<<<< HEAD
 			Filter: coralogixv1alpha1.Filter{
-=======
-			Filter: &coralogixv1alpha1.Filter{
->>>>>>> 01ad465 (chore: fixing invalid rebase)
 				WhatExpression: "source logs | filter $d.cpodId:string == '122'",
 				MetaLabels: []coralogixv1alpha1.MetaLabel{
 					{Key: "environment", Value: ptr.To("production")},
 				},
 			},
-<<<<<<< HEAD
 			Schedule: coralogixv1alpha1.Schedule{
-=======
-			Schedule: &coralogixv1alpha1.Schedule{
->>>>>>> 01ad465 (chore: fixing invalid rebase)
 				Operation: "mute",
 				Recurring: &coralogixv1alpha1.Recurring{
 					Dynamic: &coralogixv1alpha1.Dynamic{
